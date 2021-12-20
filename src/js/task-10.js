d3ea0a3e44c22cc74c4413454b9c85dc816f6c51
Lenko Yevhen <userlekko@gmail.com>
2021-12-20 13:34:44 +0200

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -7,60 +7,50 @@ const boxContainer = document.querySelector('#boxes')
 let contr = 30;
 
 
-const getANumber = (e) => {
-  return Number(e.currentTarget.value)
-}
 
-const divEl = document.createElement('div')
 const createBoxes = (amount) => {
-    for (let i = 0; i < amount.length; i++) {
-    
-  divEl.classList.add('item')
-  divEl.style.background = getRandomHexColor()
-  
-  if (boxContainer.querySelectorAll('.item').length === 0) {
-    divEl.style.height = `${30}px`
-    divEl.style.width = `${30}px`
-   boxContainer.append(divEl)
-  } else {
-    contr += 10 
+  const arrDiv = [];
+  let currentPx = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement('div')
+    divEl.classList.add('item')
+    divEl.style.backgroundColor = getRandomHexColor()
+    if (arrDiv.length === 0) {
       divEl.style.height = `${30}px`
-    divEl.style.width = `${contr}px`
-  boxContainer.append(divEl)
+      divEl.style.width = `${30}px`
+      arrDiv.push(divEl)
+    } else {
+      currentPx += 10
+      divEl.style.height = `${30}px`
+      divEl.style.width = `${currentPx}px`
+      arrDiv.push(divEl)
+    }
   }
+  
+  boxContainer.append(...arrDiv)
+  
   }
-  }
-
-controlMenu.querySelector('input').addEventListener('input', e => {
-  return e.currentTarget.value
-     
-})
-
 
 
 controlMenu.querySelector('button').addEventListener('click', e => {
-  e.preventDefault()
+  const numb = Number(controlMenu.querySelector('input').value)
+  createBoxes(numb)
+})
   
-createBoxes(Number(controlMenu.querySelector('input').value))
-
-
-  // const divEl = document.createElement('div')
-  // divEl.classList.add('item')
-  // divEl.style.background = getRandomHexColor()
-  
-  // if (boxContainer.querySelectorAll('.item').length === 0) {
-  //   divEl.style.height = `${30}px`
-  //   divEl.style.width = `${30}px`
-  //   boxContainer.append(divEl)
-  // } else {
-  //   contr += 10 
-  //     divEl.style.height = `${30}px`
-  //   divEl.style.width = `${contr}px`
-  //   boxContainer.append(divEl)
-  // }
-  
+controlMenu.querySelector('button[data-destroy]').addEventListener('click', e => {
+  const item = boxContainer.querySelectorAll('.item')
+  for (let i of item) {
+      i.remove()
+    }
   
 })
+
+
+
+  
+  
+
 
 
 
